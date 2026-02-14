29c900710622663f185855eadc12fb9a9a778b51
mzaiger <mzaiger@gmail.com>
2026-02-14 06:13:35 -0600

    Add files via upload

--- components.js
+++ components.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function () {
   const header = document.querySelector('header');
 
-  if (window.scrollY > 100) {
+  if (window.scrollY > 50) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
